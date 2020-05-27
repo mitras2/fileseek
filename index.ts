0151194318225daaff6ea4a820e904992eb48b24
L. Hermann <mitra2@web.de>
2020-05-27 08:50:35 +0200

    More robust paths by using the NodeJS path module

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import * as fs from 'fs';
+import * as nodePath from 'path';
 
 export default function fileseek(path: string, expression: RegExp, recursive: boolean = true): string[] {
     const result: string[] = [];
+
+    path = nodePath.normalize(path);
 
     if(!fs.existsSync(path)) {
         console.error('The given path does not exist');
@@ -9,7 +12,7 @@ export default function fileseek(path: string, expression: RegExp, recursive: bo
     }
 
     fs.readdirSync(path).forEach(file => {
-        const current = `${path}/${file}`;
+        const current = nodePath.join(path, file);
 
         if (fs.lstatSync(current).isDirectory() && recursive) {
             result.push(...fileseek(current, expression));
